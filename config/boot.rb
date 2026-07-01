ac028716fbd083237cd58a4bbbc9675965576a9f
Michael Hamilton <michael.ts.hamilton@gmail.com>
2026-07-01 17:54:16 -0500

    security: update core ruby gems to patched releases and configure automatic mime database resolving for macos

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV['USE_FREEDESKTOP_PLACEHOLDER'] ||= 'true'
+ENV['FREEDESKTOP_MIME_TYPES_PATH'] ||= File.expand_path('../vendor/bundle/ruby/2.6.0/gems/mimemagic-0.3.10/dummy.xml', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
